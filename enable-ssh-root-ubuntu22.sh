0dd52a332c6f5aafbb136e688e4d2d845c7fa4b7
rizky panji <rizkypanji@example.com>
2023-01-21 11:08:23 +0700

    first commit

---
+++ enable-ssh-root-ubuntu22.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo systemctl reload ssh
+sudo systemctl restart ssh
