f4ab522a01244660e1e7509c2c05816a7ebe03be
rizky panji <rizkypanji@example.com>
2023-07-12 10:46:58 +0700

    feat: add new install-jenkins file

---
+++ install-jenkins-ubuntu22.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Jenkins on ubuntu22.04
+
+sudo apt update
+sudo apt install openjdk-17-jre
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
