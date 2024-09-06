8c975f19fa2b60ab6e56d83c9846f90e4774c351
rizki.ps <rizki.ps@bts.id>
2024-09-06 09:38:19 +0700

    feat: install postgresql-client-15

---
+++ install-postgresql15-client-ubuntu22.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install -y curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt update
+sudo apt -y install postgresql-client-15
+psql --version
