8d8d2f51f17ea786a40698c64ee4e16f567f4ccd
rizkypanz16 <rizkypanji6483@gmail.com>
2024-11-07 13:27:08 +0700

    feat: install composer

---
+++ install-php-composer-ubuntu22.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer --version
