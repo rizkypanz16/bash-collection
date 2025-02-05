37d7feb5115082ed88461eae4768c99bbfeae645
rizkypanji <59678505+rizkypanz16@users.noreply.github.com>
2025-02-05 13:46:21 +0700

    feat

--- install-gcloud-cli-ubuntu22.sh
+++ install-gcloud-cli-ubuntu22.sh
@@ -5,4 +5,4 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y apt-transport-https ca-certif
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt install -y google-cloud-cli
+sudo DEBIAN_FRONTEND=noninteractive apt install -y google-cloud-cli google-cloud-sdk-gke-gcloud-auth-plugin kubectl
