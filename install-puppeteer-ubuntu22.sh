6268280c1cb102d5ecb8bb67273ae8528093d03e
rizkypanji <59678505+rizkypanz16@users.noreply.github.com>
2025-02-18 16:36:06 +0700

    feat: install-puppeteer-ubuntu22.sh

---
+++ install-puppeteer-ubuntu22.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+node -v && npm -v
+sudo apt update
+sudo apt-get install -y libx11-xcb1 libxcomposite1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 
+sudo apt-get install -y chromium-browser 
+npm install -g puppeteer 
