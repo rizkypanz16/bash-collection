961f44a9d070e565f1921fb9b5adf133fa70938e
rizkypanji <59678505+rizkypanz16@users.noreply.github.com>
2025-04-22 13:28:31 +0700

    Update install-docker-ubuntu22.sh

--- install-docker-ubuntu22.sh
+++ install-docker-ubuntu22.sh
@@ -4,6 +4,7 @@ sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/docker.gpg
 sudo apt update -y
 sudo apt install docker-ce -y
 # sudo systemctl status docker
